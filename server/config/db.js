3d8f456a9052d0de312e2fb020b44b53e2767b17
Yuvarajesh <yuvarajesh@plexarise.com>
2025-09-16 13:54:40 +0530

    fixed connection

--- server/config/db.js
+++ server/config/db.js
@@ -16,12 +16,14 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 async function connectDB() {
-    try {
-        await mongoose.connect('mongodb+srv://yuvarajesh:Yuva.V@04@cluster0.vitdzng.mongodb.net/propfixrealty?retryWrites=true&w=majority&appName=Cluster0')
-            .then(()=> console.log('Mongoo db connected with propfixrealty database'))
-    } catch (error) {
-        console.log("Mongo DB connection error:", error.message)
-    }
+  try {
+    await mongoose.connect(
+      'mongodb+srv://yuvarajesh:Yuva.V%4004@cluster0.vitdzng.mongodb.net/propfixrealty?retryWrites=true&w=majority&appName=Cluster0'
+    );
+    console.log('✅ MongoDB connected with propfixrealty database');
+  } catch (error) {
+    console.log('Mongo DB connection error:', error.message);
+  }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
