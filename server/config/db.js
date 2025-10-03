60b21948c371a0e617dbca3c694984ccce04da89
SmilyPrince <smilysathishkumar@gmail.com>
2025-10-03 18:19:04 +0530

    fix from sas

--- server/config/db.js
+++ server/config/db.js
@@ -3,14 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// fix the deprecation warnings by setting the options below
 async function connectDB() {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://localhost:27017/propfixrealty"
-    );
-    console.log("✅ MongoDB connected with localdb");
+      "mongodb+srv://yuvarajesh:Yuva.V%4004@cluster0.vitdzng.mongodb.net/propfixrealty?retryWrites=true&w=majority&appName=Cluster0"
+    )
+
+      .then(() => console.log('Mongoo db connected with propfixrealty database'))
   } catch (error) {
-    console.log("Mongo DB connection error:", error.message);
+    console.log("Mongo DB connection error:", error.message)
   }
 }
 
